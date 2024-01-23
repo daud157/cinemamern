e61cebd2ecd94047153f7e32f768708721972123
Daud <daudahmed157@gmail.com>
2024-01-23 21:37:40 +0300

    fifth commit

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app =express();
 app.use(express.json());
 app.use(cors(
     {
-        origin: [""],
+        origin: ["https://cinemamern-h1pw.vercel.app/"],
         methods: ["POST", "GET"],
         credentials: true
     }
